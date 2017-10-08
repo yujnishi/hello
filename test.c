e3636d075b71e01bffcf55852e13eccd83c804bd
yujnishi <yuji_nisimura@yahoo.co.jp>
2017-10-08 16:05:28 +0900

    Create test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <CUnit/CUnit.h>
+#include <CUnit/Console.h>
+
+void hello_test1(void) {
+    CU_ASSERT(hello() == 10);
+}
+
+int main() {
+    CU_pSuite hello_suite;
+    
+    CU_initialize_registory();
+    hello_suite = CU_add_suite("hello",NULL,NULL);
+    CU_add_test(hello_suite,"hello1",hello_test1);
+    CU_console_run_tests();
+    CU_cleanup_registory();
+    
+    return 0;
+}
+
+    
