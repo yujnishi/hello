21cdfb3e194a1c4e8f65fa1cfe9b15ecc9b0ac9f
yujnishi <yuji_nisimura@yahoo.co.jp>
2017-10-09 06:13:29 +0900

    Update test.c

--- test.c
+++ test.c
@@ -8,11 +8,11 @@ void hello_test1(void) {
 int main() {
     CU_pSuite hello_suite;
     
-    CU_initialize_registory();
+    CU_initialize_registry();
     hello_suite = CU_add_suite("hello",NULL,NULL);
     CU_add_test(hello_suite,"hello1",hello_test1);
     CU_console_run_tests();
-    CU_cleanup_registory();
+    CU_cleanup_registry();
     
     return 0;
 }
