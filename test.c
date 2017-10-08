da6fded1b569e598cc58cfce3d2bfcb534d2d33f
yujnishi <yuji_nisimura@yahoo.co.jp>
2017-10-09 06:27:51 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,5 @@
 #include <CUnit/CUnit.h>
-#include <CUnit/Console.h>
+#include <CUnit/Automated.h>
 
 void hello_test1(void) {
     CU_ASSERT(hello() == 10);
@@ -11,7 +11,7 @@ int main() {
     CU_initialize_registry();
     hello_suite = CU_add_suite("hello",NULL,NULL);
     CU_add_test(hello_suite,"hello1",hello_test1);
-    CU_console_run_tests();
+    CU_automated_run_tests();
     CU_cleanup_registry();
     
     return 0;
