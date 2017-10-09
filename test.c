3d9622dc0728d4c092ff11b629ba5bddc88b8fa0
yujnishi <yuji_nisimura@yahoo.co.jp>
2017-10-09 17:52:10 +0900

    Update test.c

--- test.c
+++ test.c
@@ -12,11 +12,11 @@ int main() {
     CU_initialize_registry();
     hello_suite = CU_add_suite("hello",NULL,NULL);
     CU_add_test(hello_suite,"hello1",hello_test1);
-       CU_basic_set_mode(CU_BRM_VERBOSE);
-   CU_basic_run_tests();
-   printf("\n");
-   CU_basic_show_failures(CU_get_failure_list());
-   printf("\n\n");
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    printf("\n");
+    CU_basic_show_failures(CU_get_failure_list());
+    printf("\n\n");
     CU_automated_run_tests();
     CU_cleanup_registry();
     
